8de8164854b14d3ec0d4089377baaff3d60ab4aa
James Pether Sörling <pethers@users.noreply.github.com>
2025-03-21 20:11:27 +0000

    refactor: remove deprecated CIAClassificationApp file and redirect to updated version

--- src/CIAClassificationApp.tsx
+++
@@ -1,7 +0,0 @@
-/**
- * @deprecated Use the version from 'src/application/CIAClassificationApp' instead.
- * This file will be removed in v1.1.0.
- */
-
-export * from "./application/CIAClassificationApp";
-export { default } from "./application/CIAClassificationApp";
